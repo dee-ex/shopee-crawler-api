bff44a808fb442f3c2f3b1b07581d4d2b7a4c7be
CdivINFx <thanhtrung2314@gmail.com>
2020-12-18 22:28:01 +0700

    feat: add api for brands

--- main.go
+++ main.go
@@ -8,20 +8,32 @@ import (
   "github.com/rs/cors"
   "github.com/gorilla/mux"
   "github.com/gorilla/handlers"
+  "github.com/dee-ex/shopee_crawler_api/modules/brands"
   "github.com/dee-ex/shopee_crawler_api/modules/jobs/trigger/crawl_brands"
   "github.com/dee-ex/shopee_crawler_api/modules/jobs/trigger/crawl_products"
 )
 
 func start_server() {
   router := mux.NewRouter().StrictSlash(true)
-  router.HandleFunc("/done", crawl_brands.HandleCrawl).Methods("GET")
-  router.HandleFunc("/", crawl_products.HandleCrawl).Methods("GET")
+
+
+  router.HandleFunc("/brands", brands.HandleCreateBrand).Methods("POST")
+  router.HandleFunc("/brands", brands.HandleGetAllBrands).Methods("GET")
+  router.HandleFunc("/brands/{brand_id}", brands.HandleGetBrandByID).Methods("GET")
+  router.HandleFunc("/brands/{brand_id}", brands.HandleUpdateByID).Methods("PUT")
+  router.HandleFunc("/brands/{brand_id}", brands.HandleDeleteByID).Methods("DELETE")
+
+  router.HandleFunc("/jobs/trigger/crawl_brands", crawl_brands.HandleCrawl).Methods("GET")
+  router.HandleFunc("/jobs/trigger/crawl_products", crawl_products.HandleCrawl).Methods("GET")
+
+
   c := cors.New(cors.Options{
     AllowedOrigins: []string{"*"},
-    AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodOptions},
+    AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
   })
   enhanced_router := c.Handler(router)
   enhanced_router = handlers.LoggingHandler(os.Stdout, enhanced_router)
+
   log.Fatal(http.ListenAndServe(":8080", enhanced_router))
 }
 
