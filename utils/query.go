d471e01c8a1608499d6e3dca6b0c14dea0cef178
CdivINFx <thanhtrung2314@gmail.com>
2020-12-19 12:42:49 +0700

    feat: add get numeric query function

---
+++ utils/query.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+  "strconv"
+  "net/http"
+)
+
+func GetNumericQuery(r *http.Request, key string, _default int) int {
+  value_query, ok := r.URL.Query()["key"]
+  if !ok {
+    return _default
+  }
+  value, err := strconv.Atoi(value_query[0])
+  if err != nil {
+    return _default
+  }
+  return value
+}
