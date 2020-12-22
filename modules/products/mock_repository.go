09c2966b0cc05f4ed85a2dd04b89f17ad187f9f3
CdivINFx <thanhtrung2314@gmail.com>
2020-12-22 17:06:24 +0700

    feat: add products tests

---
+++ modules/products/mock_repository.go
@@ -0,0 +1,85 @@
+package products
+
+import (
+  "gorm.io/gorm"
+
+  "github.com/dee-ex/shopee_crawler_api/entities"
+  "github.com/dee-ex/shopee_crawler_api/infrastructures"
+)
+
+type (
+  MockMySQL struct {
+    db *gorm.DB
+  }
+)
+
+func NewMockMySQLRepository() *MockMySQL {
+  db, err := infrastructures.NewMySQLSession("testt")
+  if err != nil {
+    return nil
+  }
+  return &MockMySQL{db: db}
+}
+
+func (repo *MockMySQL) Create(product *entities.Product) error {
+  res := repo.db.Create(product)
+  return res.Error
+}
+
+func (repo *MockMySQL) GetAllProducts(limit, offset int) ([]entities.Product, error) {
+  var products []entities.Product
+  res := repo.db.Limit(limit).Offset(offset).Find(&products)
+  return products, res.Error
+}
+
+func (repo *MockMySQL) GetProductByID(product_id int) (*entities.Product, error) {
+  var product entities.Product
+  res := repo.db.Where("id = ?", product_id).Find(&product)
+  return &product, res.Error
+}
+
+func (repo *MockMySQL) GetProductByItemid(itemid uint64) (*entities.Product, error) {
+  var product entities.Product
+  res := repo.db.Where("itemid = ?", itemid).Find(&product)
+  return &product, res.Error
+}
+
+func (repo *MockMySQL) UpdateShopid(product_id int, shopid uint64) error {
+  res := repo.db.Model(&entities.Product{}).Where("id = ?", product_id).Update("shopid", shopid)
+  return res.Error
+}
+
+func (repo *MockMySQL) UpdatePriceMax(product_id int, pricemax uint64) error {
+  res := repo.db.Model(&entities.Product{}).Where("id = ?", product_id).Update("price_max", pricemax)
+  return res.Error
+}
+
+func (repo *MockMySQL) UpdatePriceMin(product_id int, pricemin uint64) error {
+  res := repo.db.Model(&entities.Product{}).Where("id = ?", product_id).Update("price_min", pricemin)
+  return res.Error
+}
+
+func (repo *MockMySQL) UpdateName(product_id int, name string) error {
+  res := repo.db.Model(&entities.Product{}).Where("id = ?", product_id).Update("name", name)
+  return res.Error
+}
+
+func (repo *MockMySQL) UpdateImages(product_id int, images string) error {
+  res := repo.db.Model(&entities.Product{}).Where("id = ?", product_id).Update("images", images)
+  return res.Error
+}
+
+func (repo *MockMySQL) UpdateHistoricalSold(product_id int, historicalsold uint32) error {
+  res := repo.db.Model(&entities.Product{}).Where("id = ?", product_id).Update("historical_sold", historicalsold)
+  return res.Error
+}
+
+func (repo *MockMySQL) UpdateRating(product_id int, rating string) error {
+  res := repo.db.Model(&entities.Product{}).Where("id = ?", product_id).Update("rating", rating)
+  return res.Error
+}
+
+func (repo *MockMySQL) Delete(product_id int) error {
+  res := repo.db.Where("id = ?", product_id).Delete(&entities.Product{})
+  return res.Error
+}
---
+++ modules/products/service_test.go
@@ -0,0 +1,192 @@
+package products
+
+import (
+  "testing"
+)
+
+var (
+  testshopid uint64 = 999999
+  testitemid uint64 = 999998
+  testpricemax uint64 = 999997
+  testpricemin uint64 = 999996
+  testname string = "testname"
+  testimages string = "testimage"
+  testhistoricalsold uint32 = 999995
+  testrating string = "testrating"
+
+
+  updateshopid uint64 = 999994
+  updatepricemax uint64 = 999993
+  updatepricemin uint64 = 999992
+  updatename string = "updatename"
+  updateimages string = "updateimage"
+  updatehistoricalsold uint32 = 999991
+  updaterating string = "updaterating"
+)
+
+func TestCreate(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  data := ProductCreation{testshopid, testitemid, testpricemax, testpricemin, testname, testimages, testhistoricalsold, testrating}
+  _, err := serv.Create(data)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+}
+
+func TestGetAllProducts(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  _, err := serv.repo.GetAllProducts(-1, 0)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+}
+
+func TestGetProductByID(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  product, err := serv.GetProductByID(-1)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if product.ID != 0 {
+    t.Error("Select negative id")
+  }
+
+  product, err = serv.GetProductByID(999999)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if product.ID != 0 {
+    t.Error("Select non-exists id")
+  }
+}
+
+func TestGetProductByItemid(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  product, err := serv.GetProductByItemid(10)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if product.ID != 0 {
+    t.Error("Select non-exists id")
+  }
+
+  product, err = serv.GetProductByItemid(testitemid)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if product.Itemid != testitemid && product.ID != 0 {
+    t.Error("Select wrong object")
+  }
+}
+
+func TestIsDuplicateItemid(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  dup, err := serv.IsDuplicateItemid(10)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if dup {
+    t.Error("Fail to verify duplicate Itemid. FT")
+  }
+
+
+  dup, err = serv.IsDuplicateItemid(testitemid)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if !dup {
+    t.Error("Fail to verify duplicate Itemid. TF")
+  }
+}
+
+func TestUpdate(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  product, err := serv.GetProductByItemid(testitemid)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+
+  data := ProductUpdate{&updateshopid, &updatepricemax, &updatepricemin, &updatename, &updateimages, &updatehistoricalsold, &updaterating}
+  err = serv.Update(product.ID, data)
+  product, err = serv.GetProductByID(product.ID)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if product.Shopid != updateshopid {
+    t.Error("Fail to update Shopid")
+  }
+  if product.PriceMax != updatepricemax {
+    t.Error("Fail to update Pricemax")
+  }
+  if product.PriceMin != updatepricemin {
+    t.Error("Fail to update Pricemin")
+  }
+  if product.Name != updatename {
+    t.Error("Fail to update Name")
+  }
+  if product.Images != updateimages {
+    t.Error("Fail to update Images")
+  }
+  if product.HistoricalSold != updatehistoricalsold {
+    t.Error("Fail to update HistoricalSold")
+  }
+  if product.Rating != updaterating {
+    t.Error("Fail to update Rating")
+  }
+}
+
+func TestDelete(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+
+  product, err := serv.GetProductByItemid(testitemid)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+
+  err = serv.Delete(product.ID)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  product, err = serv.GetProductByID(product.ID)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if product.ID != 0 {
+    t.Error("Fail to delete product")
+  }
+}
