5a1574614d14ad5b6367aaed07f4ad976478dec0
CdivINFx <thanhtrung2314@gmail.com>
2020-12-22 17:48:22 +0700

    refactor: use Getenv and section code

--- infrastructures/mysql.go
+++ infrastructures/mysql.go
@@ -1,20 +1,23 @@
 package infrastructures
 
 import (
+  "os"
   "gorm.io/gorm"
   "gorm.io/driver/mysql"
 )
 
 func NewMySQLSession(dbname string) (*gorm.DB, error) {
-  username := "root"
-  password := "123qwe123qwe"
-  host := "localhost"
-  port := "3306"
+  username := os.Getenv("DATABASE_USERNAME")
+  password := os.Getenv("DATABASE_PASSWORD")
+  host := os.Getenv("DATABASE_HOST")
+  port := os.Getenv("DATABASE_PORT")
   dsn := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbname
+
   db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
   if err != nil {
     return nil, err
   }
+
   mysql_db, err := db.DB()
   if err != nil {
     return nil, err
@@ -22,5 +25,6 @@ func NewMySQLSession(dbname string) (*gorm.DB, error) {
   mysql_db.SetMaxOpenConns(10)
   // mysql_db.SetConnMaxIdleTime(15)
   mysql_db.SetConnMaxLifetime(15)
+
   return db, nil
 }
