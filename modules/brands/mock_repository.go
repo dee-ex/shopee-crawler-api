47661a23bdd6a18e6a7ebd1537e35934c7f6a643
CdivINFx <thanhtrung2314@gmail.com>
2020-12-22 15:51:45 +0700

    feat: add brands tests

---
+++ modules/brands/mock_repository.go
@@ -0,0 +1,72 @@
+package brands
+
+import (
+  "gorm.io/gorm"
+
+  "github.com/dee-ex/shopee_crawler_api/entities"
+  "github.com/dee-ex/shopee_crawler_api/infrastructures"
+)
+
+type (
+  MockMySQL struct {
+    db *gorm.DB
+  }
+)
+
+func NewMockMySQLRepository() *MockMySQL {
+  db, err := infrastructures.NewMySQLSession("testt")
+  if err != nil {
+    return nil
+  }
+  return &MockMySQL{db: db}
+}
+
+func (repo *MockMySQL) Create(brand *entities.Brand) error {
+  res := repo.db.Create(brand)
+  return res.Error
+}
+
+func (repo *MockMySQL) GetAllBrands() ([]entities.Brand, error) {
+  var brands []entities.Brand
+  res := repo.db.Find(&brands)
+  return brands, res.Error
+}
+
+func (repo *MockMySQL) GetBrandByID(brand_id int) (*entities.Brand, error) {
+  var brand entities.Brand
+  res := repo.db.Where("id = ?", brand_id).Find(&brand)
+  return &brand, res.Error
+}
+
+func (repo *MockMySQL) GetBrandByShopid(shopid uint64) (*entities.Brand, error) {
+  var brand entities.Brand
+  res := repo.db.Where("shopid = ?", shopid).Find(&brand)
+  return &brand, res.Error
+}
+
+func (repo *MockMySQL) GetBrandByUsername(username string) (*entities.Brand, error) {
+  var brand entities.Brand
+  res := repo.db.Where("username = ?", username).Find(&brand)
+  return &brand, res.Error
+}
+
+func (repo *MockMySQL) UpdateBrandName(brand_id int, brandname string) error {
+  res := repo.db.Model(&entities.Brand{}).Where("id = ?", brand_id).Update("brand_name", brandname)
+  return res.Error
+}
+
+func (repo *MockMySQL) UpdateLogo(brand_id int, logo string) error {
+  res := repo.db.Model(&entities.Brand{}).Where("id = ?", brand_id).Update("logo", logo)
+  return res.Error
+}
+
+func (repo *MockMySQL) Delete(brand_id int) error {
+  res := repo.db.Where("id = ?", brand_id).Delete(&entities.Brand{})
+  return res.Error
+}
+
+func (repo *MockMySQL) GetAllProducts(shopid uint64) ([]entities.Product, error) {
+  var products []entities.Product
+  res := repo.db.Where("shopid = ?", shopid).Find(&products)
+  return products, res.Error
+}
---
+++ modules/brands/service_test.go
@@ -0,0 +1,217 @@
+package brands
+
+import (
+  "testing"
+)
+
+var (
+  testshopid uint64 = 999999
+  testusername string = "testusername"
+  testbrandname string = "testbrandname"
+  testlogo string = "testlogo"
+
+  updatebrandname string = "updatebrandname"
+  updatelogo string = "updatelogo"
+)
+
+
+func TestCreate(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  data := BrandCreation{testshopid, testusername, testbrandname, testlogo}
+  _, err := serv.Create(data)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+}
+
+func TestGetAllBrands(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  _, err := serv.GetAllBrands()
+  if err != nil {
+    t.Fatal("Server error")
+  }
+}
+
+func TestGetBrandByID(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  brand, err := serv.GetBrandByID(-1)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if brand.ID != 0 {
+    t.Error("Select negative id")
+  }
+
+  brand, err = serv.GetBrandByID(999999)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if brand.ID != 0 {
+    t.Error("Select non-exists id")
+  }
+}
+
+func TestGetBrandByShopid(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  brand, err := serv.GetBrandByShopid(10)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if brand.ID != 0 {
+    t.Error("Select non-exists id")
+  }
+
+  brand, err = serv.GetBrandByShopid(testshopid)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if brand.Shopid != testshopid && brand.ID != 0 {
+    t.Error("Select wrong object")
+  }
+}
+
+func TestGetBrandByUsername(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  brand, err := serv.GetBrandByUsername("non-exists_username")
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if brand.ID != 0 {
+    t.Error("Select non-exists username")
+  }
+
+
+  brand, err = serv.GetBrandByUsername(testusername)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if brand.Username != testusername && brand.ID != 0 {
+    t.Error("Select wrong object")
+  }
+}
+
+func TestIsDuplicateShopid(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  dup, err := serv.IsDuplicateShopid(10)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if dup {
+    t.Error("Fail to verify duplicate Shopid. FT")
+  }
+
+  dup, err = serv.IsDuplicateShopid(testshopid)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if !dup {
+    t.Error("Fail to verify duplicate Shopid. TF")
+  }
+}
+
+func TestIsDuplicateUsername(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  dup, err := serv.IsDuplicateUsername("non-exists_username")
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if dup {
+    t.Error("Fail to verify duplicate Username. FT")
+  }
+
+
+  dup, err = serv.IsDuplicateUsername(testusername)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if !dup {
+    t.Error("Fail to verify duplicate Username. TF")
+  }
+}
+
+func TestUpdate(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+  brand, err := serv.GetBrandByUsername(testusername)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+
+  data := BrandUpdate{&updatebrandname, &updatelogo}
+  err = serv.Update(brand.ID, data)
+  brand, err = serv.GetBrandByID(brand.ID)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if brand.BrandName != updatebrandname {
+    t.Error("Fail to update BrandName")
+  }
+  if brand.Logo != updatelogo {
+    t.Error("Fail to update Logo")
+  }
+}
+
+func TestDelete(t *testing.T) {
+  mock := NewMockMySQLRepository()
+  if mock == nil {
+    t.Fatal("Cannot connect to database")
+  }
+  serv := NewService(mock)
+
+
+  brand, err := serv.GetBrandByUsername(testusername)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+
+  err = serv.Delete(brand.ID)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  brand, err = serv.GetBrandByID(brand.ID)
+  if err != nil {
+    t.Fatal("Server error")
+  }
+  if brand.ID != 0 {
+    t.Error("Fail to delete brand")
+  }
+}
